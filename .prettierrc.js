021df69bf2f770166f21a2e978d01f6dc328b5ab
Leo Hui <leohxj@gmail.com>
2019-08-30 11:56:34 +0800

    feat: use @mjolnir/prettier-config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,18 +1,6 @@
 'use strict';
 
-// module.exports = require("@mjolnir/prettier-config");
-
 module.exports = {
-  printWidth: 100,
-  singleQuote: true,
-  trailingComma: 'all',
-  htmlWhitespaceSensitivity: 'ignore',
-  overrides: [
-    {
-      files: ['.*rc', '*.json'],
-      options: {
-        parser: 'json-stringify',
-      },
-    },
-  ],
-};
+  ...require("@mjolnir/prettier-config")
+  // override edge case
+}
